e7599e26af8f1b58c3076269b017c79044933a2b
care0717 <dev.care0717@gmail.com>
2021-01-04 20:32:37 +0900

    feat: impl builtin func for array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -17,4 +17,68 @@ var builtins = map[string]*object.Builtin{
 			return newError("argument to `len` not supported, got %s", arg.Type())
 		}
 	}},
+	"first": {Fn: func(args ...object.Object) object.Object {
+		if len(args) != 1 {
+			return newError("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		switch arg := args[0].(type) {
+		case *object.Array:
+			if len(arg.Elements) > 0 {
+				return arg.Elements[0]
+			} else {
+				return object.NULL
+			}
+		default:
+			return newError("argument to `first` not supported, got %s", arg.Type())
+		}
+	}},
+	"last": {Fn: func(args ...object.Object) object.Object {
+		if len(args) != 1 {
+			return newError("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		switch arg := args[0].(type) {
+		case *object.Array:
+			length := len(arg.Elements)
+			if length > 0 {
+				return arg.Elements[length-1]
+			} else {
+				return object.NULL
+			}
+		default:
+			return newError("argument to `last` not supported, got %s", arg.Type())
+		}
+	}},
+	"rest": {Fn: func(args ...object.Object) object.Object {
+		if len(args) != 1 {
+			return newError("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		switch arg := args[0].(type) {
+		case *object.Array:
+			length := len(arg.Elements)
+			newElements := make([]object.Object, length-1, length-1)
+			copy(newElements, arg.Elements[1:length])
+			return &object.Array{Elements:newElements}
+		default:
+			return newError("argument to `rest` not supported, got %s", arg.Type())
+		}
+	}},
+	"push": {Fn: func(args ...object.Object) object.Object {
+		if len(args) != 2 {
+			return newError("wrong number of arguments. got=%d, want=2", len(args))
+		}
+
+		switch arg := args[0].(type) {
+		case *object.Array:
+			length := len(arg.Elements)
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arg.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements:newElements}
+		default:
+			return newError("argument to `push` not supported, got %s", arg.Type())
+		}
+	}},
 }
